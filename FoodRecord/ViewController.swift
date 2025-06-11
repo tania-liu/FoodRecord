f5581686076e845ec1af0031de589a95c4e7d6e5
Tania Liu <tanialiu430006@gmail.com>
2025-06-11 17:12:07 +0800

    Initial Commit

---
+++ FoodRecord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodRecord
+//
+//  Created by 劉俐廷 on 2025/6/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
